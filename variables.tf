e27de04c9c0a9712e9babbff9369082b33972932
lanceplarsen <lancep.larsen43@gmail.com>
2018-03-20 17:39:53 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "cluster_name" {
 
 variable "initial_node_count" {
   description = "Number of worker VMs to initially create"
-  default = 1
+  default = 6
 }
 
 variable "master_username" {
